39e61a4f9b3d56efcf7226ce3cb40a9775bacf7d
Mika Paulasaari <mika.paulasaari@idean.com>
2019-03-17 00:40:37 +0200

    Add data parsing

---
+++ app/src/utils/dataParsing.js
@@ -0,0 +1,33 @@
+import * as R from 'ramda'
+
+const STOCK_KEYS = [
+  'description',
+  'latestPrice',
+  'symbol',
+]
+
+export const parseStockData = (stock) => {
+  const props = {}
+
+  Object.keys(stock).forEach((key) => {
+    if (STOCK_KEYS.includes(key)) {
+      props[key] = stock[key]
+    }
+  })
+
+  return props
+}
+
+export const getUpdatedList = (list, symbol, update) => {
+  const itemIndex = R.findIndex(R.propEq('symbol', symbol))(list)
+
+  if (itemIndex >= 0) {
+    return R.adjust(
+      itemIndex,
+      stock => ({ ...stock, ...parseStockData(update) }),
+      list,
+    )
+  }
+
+  return [...list, update]
+}
